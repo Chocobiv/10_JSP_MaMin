9f41b29f2c90af466989dab4bf59e600d66af2f5
Chocobiv <47735101+Chocobiv@users.noreply.github.com>
2022-10-27 18:10:19 +0900

    [22/10/27 18:09] 회원가입,로그인 개발 중

---
+++ mamin/src/main/java/model/dao/MemberDao.java
@@ -0,0 +1,63 @@
+package model.dao;
+
+import model.dto.MemberDto;
+
+public class MemberDao extends Dao {
+
+	// singleton - 메모리 하나만 쓰기 위해서 [공유 메모리]
+	private static MemberDao mdao = new MemberDao();
+
+	public static MemberDao getInstance() {
+		return mdao;
+	}
+
+	// 비아 - 1. 회원가입 메소드
+	public boolean signup(MemberDto dto) {
+		String sql = "insert into member (m_id,m_password,m_email,m_nick,m_img,m_profile) values(?,?,?,?,?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, dto.getM_id());
+			ps.setString(2, dto.getM_password());
+			ps.setString(3, dto.getM_email());
+			ps.setString(4, dto.getM_nick());
+			ps.setString(5, dto.getM_img());
+			ps.setString(6, dto.getM_profile());
+			ps.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return false;
+	}
+
+	// 비아 - 2. 아이디 중복체크
+	public boolean idcheck(String m_id) {
+		String sql = "select * from member where m_id=?";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, m_id);
+			rs = ps.executeQuery();
+			if (rs.next())
+				return true;
+		} catch (Exception e) {
+			System.out.println("DB오류) " + e);
+		}
+		return false;
+	}
+
+	// 비아 - 3. 이메일 중복체크
+	public boolean emailcheck(String m_email) {
+		String sql = "select * from member where m_email=?";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, m_email);
+			rs = ps.executeQuery();
+			if (rs.next())
+				return true;
+		} catch (Exception e) {
+			System.out.println("DB오류) " + e);
+		}
+		return false;
+	}
+
+}
